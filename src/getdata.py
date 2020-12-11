2ebd74e8d874c91790e86bdd4548c873baed4977
patri-carrasco <patri.carrasco@gmail.com>
2020-12-11 17:07:21 +0100

    creada get_title

--- src/getdata.py
+++ src/getdata.py
@@ -3,7 +3,7 @@ from config.configuration import conn
 
 def get_genre(genre):
   """
-      
+      Función que dado un tipo de género de película da el que mejor rating tiene
       arg:
         genre = genero de la película
       return:
@@ -15,5 +15,25 @@ def get_genre(genre):
   film = list(conn.execute(query))[0]
   film_dict['film']= film[0]
   film_dict['ratings'] = film[1]
+  
+  return film_dict
+
+
+def get_title(title):
+  """
+      Función que dado un tipo de título de película devuleva el de género con más ratings
+      arg:
+        genre = genero de la película
+      return:
+       film = titulo película
+  """
+   
+  film_dict = {}
+
+  query = f"SELECT genre FROM title_movies WHERE title = '{title}';"
+  
+  genre = list(conn.execute(query))
+  print(f' fkgjaflñkga {genre[0][0]}')
+  film_dict = get_genre(genre[0][0])
   
   return film_dict
