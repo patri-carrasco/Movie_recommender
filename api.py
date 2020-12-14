4098fc83fccf35518618ba73dacd4b7297075ef9
patri-carrasco <patri.carrasco@gmail.com>
2020-12-14 19:32:32 +0100

    Creado enpoint para el recomendador

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from flask import request
 import markdown.extensions.fenced_code
 
 import  src.getdata as get
-#import src.posdata  as pos
+import src.recommender  as rec
 #import src.sentiment as sen
 import json
 
@@ -38,7 +38,10 @@ def get_title(title):
 
 
 
-
+@app.route("/recommender/<title>")
+def recommender_system(title):
+  titles = rec.get_recommendations(title)
+  return json.dumps(titles)
 
 
 
