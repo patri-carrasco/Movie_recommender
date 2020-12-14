2fd962f76220a768ef186167b9e1b0418593d2fb
patri-carrasco <patri.carrasco@gmail.com>
2020-12-14 16:17:24 +0100

    Creada la que limita los datos del recomendador

--- src/clean_data_recommender.py
+++ src/clean_data_recommender.py
@@ -4,21 +4,31 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-#Cargamos lo datos de IMDb movies.csv
-data = pd.read_csv("./data/IMDb movies.csv",low_memory=False)
+def clean_data_recommender():
+    #Cargamos lo datos de IMDb movies.csv
+    data = pd.read_csv("./data/IMDb movies.csv",low_memory=False)
 
-# Borramos columnas innecesarias
-data = data.drop(['usa_gross_income', 'worlwide_gross_income','metascore','reviews_from_users','reviews_from_critics'], axis=1)
-data = data.drop(['production_company','budget','production_company','writer'],axis = 1)
+    # Borramos columnas innecesarias
+    data = data.drop(['usa_gross_income', 'worlwide_gross_income','metascore','reviews_from_users','reviews_from_critics'], axis=1)
+    data = data.drop(['production_company','budget','production_company','writer'],axis = 1)
 
-#También borramos todos los datos nulos de las columnas. 
-data = data.dropna(subset=['actors', 'country','language','director','description'])
+    #También borramos todos los datos nulos de las columnas. 
+    data = data.dropna(subset=['actors', 'country','language','director','description'])
 
-# Nos quedamos sólo con el país  USA
-data_usa = data[data.country == 'USA']
+    #Quitamos las películas del país India
+    data = data.drop(data[data['country']=='India'].index)
 
-# Mejor ratings mayor que 6
-data_usa_best = data_usa[data_usa.avg_vote > 6]
 
-# Guardamos los datos
-data_usa_best.to_csv("../data/data_usa_best.csv")
+    # películas a partir de los años 1960
+    data = data.drop(data[data['year'] < '1960'].index)
+
+
+    # películas de ratings superior a 6.7
+    data = data.drop(data[data['avg_vote'] < 6.7].index)
+
+
+
+    data.reset_index(inplace=True, drop=True)
+
+    # Guardamos los datos
+    data.to_csv("../data/data.csv")
